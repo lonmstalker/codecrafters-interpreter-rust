8e72c313b6c316626f4f725cda028ac190fdd5f5
lonmstalker <nikitakocnev@MacBook-Pro-Nikita.local>
2025-02-21 23:43:58 +0300

    complete task

---
+++ src/lexer.rs
@@ -0,0 +1,43 @@
+use std::{fs, io};
+
+pub fn tokenize(filename: &String) -> i32 {
+
+    let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
+        writeln!(io::stderr(), "Failed to read file {}", filename).unwrap();
+        String::new()
+    });
+
+    process_tokens(file_contents)
+}
+
+fn process_tokens(code: String) -> i32 {
+    let mut line = 1;
+
+    if !code.is_empty() {
+        for c in code.chars() {
+            match c {
+                '(' => println!("LEFT_PAREN ( null"),
+                ')' => println!("RIGHT_PAREN ) null"),
+                '{' => println!("LEFT_BRACE {{ null"),
+                '}' => println!("RIGHT_BRACE }} null"),
+                ',' => println!("COMMA , null"),
+                '.' => println!("DOT . null"),
+                '+' => println!("PLUS + null"),
+                '-' => println!("MINUS - null"),
+                ';' => println!("SEMICOLON ; null"),
+                '*' => println!("STAR * null"),
+                '\n' => line += 1,
+                '$' | '#' => {
+                    eprintln!("[line {}] Error: Unexpected character: {}", line, c);
+                    return 65
+                }
+                _ => {}
+            }
+        }
+        println!("EOF  null");
+    } else {
+        println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
+    }
+
+    0
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+mod lexer;
+
 use std::env;
-use std::fs;
 use std::io::{self, Write};
+use std::process::exit;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,45 +14,15 @@ fn main() {
     let command = &args[1];
     let filename = &args[2];
 
-    match command.as_str() {
-        "tokenize" => {
-            // You can use print statements as follows for debugging, they'll be visible when running tests.
-            writeln!(io::stderr(), "Logs from your program will appear here!").unwrap();
-
-            let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
-                writeln!(io::stderr(), "Failed to read file {}", filename).unwrap();
-                String::new()
-            });
-
-            let mut line = 1;
-
-            if !file_contents.is_empty() {
-                for c in file_contents.chars() {
-                    match c {
-                        '(' => println!("LEFT_PAREN ( null"),
-                        ')' => println!("RIGHT_PAREN ) null"),
-                        '{' => println!("LEFT_BRACE {{ null"),
-                        '}' => println!("RIGHT_BRACE }} null"),
-                        ',' => println!("COMMA , null"),
-                        '.' => println!("DOT . null"),
-                        '+' => println!("PLUS + null"),
-                        '-' => println!("MINUS - null"),
-                        ';' => println!("SEMICOLON ; null"),
-                        '*' => println!("STAR * null"),
-                        '\n' => line += 1,
-                        '$' => eprintln!("[line {}] Error: Unexpected character: $", line),
-                        '#' => eprintln!("[line {}] Error: Unexpected character: #", line),
-                        _ => {}
-                    }
-                }
-                println!("EOF  null");
-            } else {
-                println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
-            }
-        }
+    let code = match command.as_str() {
+        "tokenize" => lexer::tokenize(filename),
         _ => {
             writeln!(io::stderr(), "Unknown command: {}", command).unwrap();
-            return;
+            0
         }
+    };
+
+    if code != 0 {
+        exit(code);
     }
 }
