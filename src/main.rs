02ab289d8e9b45e097cc92e173e383df63858152
lonmstalker <nikitakocnev@MacBook-Pro-Nikita.local>
2025-02-21 23:49:39 +0300

    complete task

--- src/main.rs
+++ src/main.rs
@@ -15,14 +15,12 @@ fn main() {
     let filename = &args[2];
 
     let code = match command.as_str() {
-        "tokenize" => lexer::tokenize(filename),
+        "tokenize" => {
+            exit(lexer::tokenize(filename))
+        }
         _ => {
             writeln!(io::stderr(), "Unknown command: {}", command).unwrap();
             0
         }
     };
-
-    if code != 0 {
-        exit(code);
-    }
 }
