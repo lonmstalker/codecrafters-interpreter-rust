40f74370886b6d5d560e2c9560a0c16973e9e57d
lonmstalker <nikitakocnev@MacBook-Pro-Nikita.local>
2025-02-22 00:18:29 +0300

    complete task

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,4 +1,5 @@
 use std::{fs, io};
+use std::fmt::Display;
 use std::io::Write;
 
 pub fn tokenize(filename: &String) -> i32 {
@@ -16,20 +17,33 @@ pub fn tokenize(filename: &String) -> i32 {
 fn process_tokens(code: String) -> i32 {
     let mut line = 1;
     let mut result = 0;
+    let mut tokens = Vec::new();
+    let mut data = code.chars().peekable();
 
     if !code.is_empty() {
-        for c in code.chars() {
+        while let Some(c) = data.next() {
             match c {
-                '(' => println!("LEFT_PAREN ( null"),
-                ')' => println!("RIGHT_PAREN ) null"),
-                '{' => println!("LEFT_BRACE {{ null"),
-                '}' => println!("RIGHT_BRACE }} null"),
-                ',' => println!("COMMA , null"),
-                '.' => println!("DOT . null"),
-                '+' => println!("PLUS + null"),
-                '-' => println!("MINUS - null"),
-                ';' => println!("SEMICOLON ; null"),
-                '*' => println!("STAR * null"),
+                '(' => tokens.push(Token::new(TokenType::LEFT_PAREN, c.to_string())),
+                ')' => tokens.push(Token::new(TokenType::RIGHT_PAREN, c.to_string())),
+                '{' => tokens.push(Token::new(TokenType::LEFT_BRACE, c.to_string())),
+                '}' => tokens.push(Token::new(TokenType::RIGHT_BRACE, c.to_string())),
+                ',' => tokens.push(Token::new(TokenType::COMMA, c.to_string())),
+                '.' => tokens.push(Token::new(TokenType::DOT, c.to_string())),
+                '+' => tokens.push(Token::new(TokenType::PLUS, c.to_string())),
+                '-' => tokens.push(Token::new(TokenType::MINUS, c.to_string())),
+                ';' => tokens.push(Token::new(TokenType::SEMICOLON, c.to_string())),
+                '*' => tokens.push(Token::new(TokenType::STAR, c.to_string())),
+                '=' => {
+                    let next = data.peek();
+
+                    if next.is_some() && *next.unwrap() == '=' {
+                        tokens.push(Token::new(TokenType::EQUAL_EQUAL, "==".to_string()));
+                        data.next();
+                    } else {
+                        tokens.push(Token::new(TokenType::EQUAL, c.to_string()));
+                    }
+
+                }
                 '\n' => line += 1,
                 unknown => {
                     eprintln!("[line {}] Error: Unexpected character: {}", line, unknown);
@@ -38,5 +52,57 @@ fn process_tokens(code: String) -> i32 {
             }
         }
     }
+
+    for token in tokens {
+        println!("{:?} {} null", token._type, token._value.unwrap_or_else(|| "".to_string()))
+    }
+
     result
+}
+
+pub struct Token {
+    _type: TokenType,
+    _string: String,
+    _value: Option<String>,
+}
+
+impl Token {
+    pub fn new(_type: TokenType, _string: String) -> Self {
+        Token {
+            _type,
+            _string,
+            _value: None,
+        }
+    }
+}
+
+impl Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{:?} {} {}",
+            self._type,
+            self._string,
+            self._value.clone().unwrap_or("null".to_string())
+        )
+    }
+}
+
+#[derive(Debug)]
+#[allow(non_camel_case_types)]
+enum TokenType {
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    COMMA,
+    DOT,
+    MINUS,
+    PLUS,
+    SEMICOLON,
+    STAR,
+    FOWARD_SLASH,
+    EQUAL,
+    EQUAL_EQUAL,
+    EOF,
 }
